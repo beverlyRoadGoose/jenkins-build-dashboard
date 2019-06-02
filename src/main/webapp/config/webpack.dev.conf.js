909b83f76de3069b4421bd32c26525b78ea771e8
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-06-03 00:04:27 +0200

    Reformat webpack config files

--- src/main/webapp/config/webpack.dev.conf.js
+++ src/main/webapp/config/webpack.dev.conf.js
@@ -30,86 +30,86 @@ const resolve = relativePath => path.resolve(__dirname, '..', relativePath);
 
 module.exports = {
 
-    mode: 'development',
+  mode: 'development',
 
-    entry: {
-        vue: 'vue',
-        index: resolve('src/webapp.js'),
-    },
+  entry: {
+    vue: 'vue',
+    index: resolve('src/webapp.js'),
+  },
 
-    output: {
-        filename: 'webapp.js',
-        path: resolve('webapp'),
-        publicPath: '../webapp/',
-    },
+  output: {
+    filename: 'webapp.js',
+    path: resolve('webapp'),
+    publicPath: '../webapp/',
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        css: ['vue-style-loader', {
-                            loader: 'css-loader',
-                        }],
-                        js: [
-                            'babel-loader',
-                        ],
-                    },
-                    cacheBusting: true,
-                },
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                include: [
-                    resolve('app'),
-                    resolve('node_modules/webpack-dev-server/client'),
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                        }
-                    },
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
-    },
-
-    plugins: [
-        new VueLoaderPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-
-    resolve: {
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            css: ['vue-style-loader', {
+              loader: 'css-loader',
+            }],
+            js: [
+              'babel-loader',
+            ],
+          },
+          cacheBusting: true,
         },
-        extensions: ['*', '.vue', '.js', '.json', '.css'],
-    },
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [
+          resolve('app'),
+          resolve('node_modules/webpack-dev-server/client'),
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
 
-    performance: {
-        hints: false,
-    }
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+    },
+    extensions: ['*', '.vue', '.js', '.json', '.css'],
+  },
+
+  performance: {
+    hints: false,
+  }
 
 };
