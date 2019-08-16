2ba8cbc6ec0edad21910b98087349a45ba85483e
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-08-16 17:32:37 +0200

    pipeline support wip

---
+++ src/main/kotlin/me/tobiadeyinka/jenkinsci/plugins/builddashboard/plugins/PluginManager.kt
@@ -0,0 +1,50 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Oluwatobi Adeyinka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package me.tobiadeyinka.jenkinsci.plugins.builddashboard.plugins
+
+import jenkins.model.Jenkins
+
+import java.util.*
+
+class PluginManager {
+
+    fun rebuildPluginIsInstalled(): Boolean = pluginIsInstalled(SupportedPlugins.REBUILD.artifactId)
+
+    fun pipelinePluginIsInstalled(): Boolean = pluginIsInstalled(SupportedPlugins.PIPELINE.artifactId)
+
+    /**
+     * Checks if a plugin is installed on the jenkins server
+     *
+     * @param[artifactId] Jenkins artifactid/shortname for the plugin
+     *
+     * @return true if the plugin is installed, false otherwise
+     */
+    private fun pluginIsInstalled(artifactId: String): Boolean = !Objects.isNull(
+        Jenkins.getInstance()
+        ?.getPlugin(artifactId)
+    )
+
+}
