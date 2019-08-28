6ff7cff9a25936cad15c004387f00bd59a9a6996
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-08-28 18:46:32 +0200

    Formatting

--- src/main/kotlin/me/tobiadeyinka/jenkinsci/plugins/builddashboard/security/csrf/RemoteRequestCrumb.kt
+++ src/main/kotlin/me/tobiadeyinka/jenkinsci/plugins/builddashboard/security/csrf/RemoteRequestCrumb.kt
@@ -1,6 +1,5 @@
 package me.tobiadeyinka.jenkinsci.plugins.builddashboard.security.csrf
 
-
 import jenkins.model.Jenkins
 import hudson.security.csrf.CrumbIssuer
 import com.fasterxml.jackson.annotation.JsonIgnore
